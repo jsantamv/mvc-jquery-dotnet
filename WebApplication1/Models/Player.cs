55069bae93cd75368bfac6367bcd241414aa12c9
Juan Carlos Santamaria V <jsantamv@live.com>
2022-08-08 09:57:08 -0600

    Update

---
+++ WebApplication1/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class PlayerClaim
+    {       
+         public string WrongClaims { get; set; }
+         public string ClaimResponses { get; set; }
+         public int TicketId { get; set; }
+         public DateTime DateClaim { get; set; }
+         public DateTime StartDate { get; set; }
+         public DateTime EndDate { get; set; }
+         public string Player { get; set; }
+         public string Agent { get; set; }
+         public string Status { get; set; }
+    }
+}
---
+++ WebApplication1/Scripts/main.js
@@ -0,0 +1,33 @@
+﻿
+
+const getData = () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    //var body = JSON.stringify({
+    //    "StartDate": "2022-01-01",
+    //    "EndDate": "2022-01-01",
+    //    "aaNAME": "VT04000",
+    //    "TicketId": "22",
+    //    "Status": "Rejected"
+    //});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify({
+            "StartDate": "2022-01-01",
+            "EndDate": "2022-01-01",
+            "aaNAME": "VT04000",
+            "TicketId": "22",
+            "Status": "Rejected"
+        })
+    };
+
+    console.log(requestOptions);
+
+    fetch("/Home/List", requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
